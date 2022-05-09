24b553c58c9dc7e66818a13a17e810bf7830a42c
CJ Koshiol <105226241+cj-snowflake-training@users.noreply.github.com>
2022-05-09 10:30:00 -0500

    Allow the user to select fruits
    Added the multiselect so that the user can pick certain fruits from the list and display them in the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# fruit list from S3 bucket
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+# allow the user to pick some fruits from the list
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+
+# display the table on the page
 streamlit.dataframe(my_fruit_list)
