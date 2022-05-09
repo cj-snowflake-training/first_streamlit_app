97dcad2b87267465cfc5299bebf65c31ba619af7
CJ Koshiol <105226241+cj-snowflake-training@users.noreply.github.com>
2022-05-09 11:12:32 -0500

    Fixed it again
    I think this will work now

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # fruit list from S3 bucket
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # allow the user to pick some fruits from the list
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Banana', 'Strawberries'])
