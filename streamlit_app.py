1f24ee1a7371a4b3cab18d860276a2149b44571f
CJ Koshiol <105226241+cj-snowflake-training@users.noreply.github.com>
2022-05-09 11:05:29 -0500

    Updated app to only show selected fruits
    Added functionality to only show the fruits that are selected by the customer for their smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # fruit list from S3 bucket
-my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # allow the user to pick some fruits from the list
 streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Banana', 'Strawberries'])
