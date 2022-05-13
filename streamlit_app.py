7d22d6b95cdb29bc5734769af7098772e674e582
CJ Koshiol <105226241+cj-snowflake-training@users.noreply.github.com>
2022-05-13 10:57:24 -0700

    Demo control of flow concept issue
    Demo control of flow concept issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,6 @@ streamlit.dataframe(my_data_rows)
 # allow a user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
+# insert the fruit into the fruit load list
+# THIS WON'T WORK BUT GO WITH IT FOR THE DEMO AND LEARNING
+my_cur.execute("INSERT INTO fruit_load_list VALUES ('from streamlit')")
